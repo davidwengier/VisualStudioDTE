cec24b9adc6d6b5ed82c6c2d3ab323558e0a6a74
David Wengier <git@wengier.com>
2019-03-01 09:18:25 +1100

    Add project files.

---
+++ VisualStudioDTE/AutomateVS.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using EnvDTE;
+
+namespace QueryVisualStudioProperties
+{
+    // From https://blogs.msdn.microsoft.com/kirillosenkov/2011/08/10/how-to-get-dte-from-visual-studio-process-id/
+    // Slightly modified to find a random Visual Studio instance, instead of a specific one.
+    public static class AutomateVS
+    {
+        [DllImport("ole32.dll")]
+        private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+
+        public static DTE FindDTE()
+        {
+            string progId = "!VisualStudio.DTE.16.0:";
+            object runningObject = null;
+
+            IBindCtx bindCtx = null;
+            IRunningObjectTable rot = null;
+            IEnumMoniker enumMonikers = null;
+
+            try
+            {
+                Marshal.ThrowExceptionForHR(CreateBindCtx(reserved: 0, ppbc: out bindCtx));
+                bindCtx.GetRunningObjectTable(out rot);
+                rot.EnumRunning(out enumMonikers);
+
+                IMoniker[] moniker = new IMoniker[1];
+                IntPtr numberFetched = IntPtr.Zero;
+                while (enumMonikers.Next(1, moniker, numberFetched) == 0)
+                {
+                    IMoniker runningObjectMoniker = moniker[0];
+
+                    string name = null;
+
+                    try
+                    {
+                        if (runningObjectMoniker != null)
+                        {
+                            runningObjectMoniker.GetDisplayName(bindCtx, null, out name);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        // Do nothing, there is something in the ROT that we do not have access to.
+                    }
+
+                    if (!string.IsNullOrEmpty(name) && name.StartsWith(progId, StringComparison.Ordinal))
+                    {
+                        Marshal.ThrowExceptionForHR(rot.GetObject(runningObjectMoniker, out runningObject));
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                if (enumMonikers != null)
+                {
+                    Marshal.ReleaseComObject(enumMonikers);
+                }
+
+                if (rot != null)
+                {
+                    Marshal.ReleaseComObject(rot);
+                }
+
+                if (bindCtx != null)
+                {
+                    Marshal.ReleaseComObject(bindCtx);
+                }
+            }
+
+            return (DTE)runningObject;
+        }
+
+    }
+}
---
+++ VisualStudioDTE/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+
+namespace QueryVisualStudioProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dte = AutomateVS.FindDTE();
+
+            // Make sure we can talk to it.. this is just here to throw an exception early
+            dte.StatusBar.Text = "Hello World!";
+
+            foreach (var proj in dte.ActiveSolutionProjects)
+            {
+                EnvDTE.Project project = proj as EnvDTE.Project;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(project.Name + ":");
+                Console.WriteLine("Automatic:");
+                Console.ResetColor();
+                EnvDTE.Properties properties = project.Properties;
+                foreach (var prop in properties)
+                {
+                    WriteProperty(prop);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Manually:");
+                Console.ResetColor();
+                // Make sure we can get some by name
+                WriteProperty(properties, "OutputType");
+                WriteProperty(properties, "OutputTypeEx");
+                WriteProperty(properties, "TargetFramework");
+                WriteProperty(properties, "TargetFrameworkMoniker");
+                WriteProperty(properties, "TargetFrameworkMonikers");
+                WriteProperty(properties, "Authors");
+                WriteProperty(properties, "PreBuildEvent");
+                WriteProperty(properties, "PostBuildEvent");
+                WriteProperty(properties, "ApplicationManifest");
+                WriteProperty(properties, "PackageId");
+                WriteProperty(properties, "Authors");
+                WriteProperty(properties, "AssemblyOriginatorKeyFile");
+            }
+        }
+
+        private static void WriteProperty(Properties properties, string propName)
+        {
+            try
+            {
+                var prop = properties.Item(propName);
+                if (prop == null)
+                {
+                    Console.WriteLine(propName + ": Couldn't find property");
+                }
+                else
+                {
+                    WriteProperty(prop);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(propName + ": ERROR: " + ex.Message);
+                Console.ResetColor();
+            }
+        }
+
+        private static void WriteProperty(object prop)
+        {
+            EnvDTE.Property property = prop as EnvDTE.Property;
+            Console.Write("  " + property.Name + ": ");
+            try
+            {
+                var value = (object)property.Value;
+                if (value == null)
+                {
+                    Console.WriteLine("[null]");
+                }
+                else
+                {
+                    Console.Write("(" + value.GetType().Name + ") ");
+                    Console.WriteLine(value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR: " + ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
